4036b6179f6c98737d6db99f6d0c79f7ff99d195
LeinerViloria <88936718+LeinerViloria@users.noreply.github.com>
2021-10-22 11:47:34 -0500

    Un poco mas cerca

--- js/compra.js
+++ js/compra.js
@@ -12,41 +12,68 @@ function agregar(){
         var imagen = document.getElementById("imagen");
         var descripcion = document.getElementById("descripcionProducto").value;
         
-        //Se crea la fila
-        const fila = document.createElement("tr");
+        if(codigo!="" && nombre!="" && cantidad!="" && precioCompra!="" && precioVenta!="" && imagen.value!="" && descripcion!="" && familia!=""){
+            //Se crea la fila
+            const fila = document.createElement("tr");
 
-        //Se crea cada columna
-        const col_codigo = document.createElement("td");
-        col_codigo.className="product-code";
-        col_codigo.textContent=codigo;
+            //Se crea cada columna
+            const col_codigo = document.createElement("td");
+            col_codigo.className="product-code";
+            col_codigo.textContent=codigo;
 
-        const col_imagen = document.createElement("td");
-        col_imagen.className="product-image";
+            const col_imagen = document.createElement("td");
+            col_imagen.className="product-image";
 
-        const col_nombre = document.createElement("td");
-        col_nombre.className="product-name";
-        col_nombre.textContent=nombre;
+            const col_nombre = document.createElement("td");
+            col_nombre.className="product-name";
+            col_nombre.textContent=nombre;
 
-        const col_precioC = document.createElement("td");
-        col_precioC.className="product-priceC";
-        col_precioC.textContent=precioCompra;
+            const col_precioC = document.createElement("td");
+            col_precioC.className="product-priceC";
+            col_precioC.textContent=precioCompra;
 
-        const col_cant = document.createElement("td");
-        col_cant.className="product-stock";   
-        col_cant.textContent=cantidad;
-        
-        //En el siguiente td se contendrá los input hidden del formulario
-        const col_apartado = document.createElement("td");
-        col_apartado.className="product-remove";
+            const col_cant = document.createElement("td");
+            col_cant.className="product-stock";   
+            col_cant.textContent=cantidad;
 
-        fila.appendChild(col_codigo);
-        fila.appendChild(col_imagen);
-        fila.appendChild(col_nombre);
-        fila.appendChild(col_precioC);
-        fila.appendChild(col_cant);
-        fila.appendChild(col_apartado);
+            //Se va a crear un solo input con todos las variables en un array
+            var variables =
+            {'codigo':codigo, 'familia':familia, 'imagen':imagen.value, 'nombre':nombre, 'cantidad':cantidad, 'precioCompra':precioCompra, 'precioVenta':precioVenta, 'precioVenta':precioVenta, 'descripcion':descripcion};                                
+            
+            //En el siguiente td se contendrá los input hidden del formulario
+            const col_apartado = document.createElement("td");
+            col_apartado.className="product-remove";
 
-        tbody.appendChild(fila);
+            for(var indice in variables){
+                const input = document.createElement("input");
+                input.type="hidden";
+                input.name="fila[]";
+                input.value=variables[indice];
+
+                col_apartado.appendChild(input);
+            }
+
+            fila.appendChild(col_codigo);
+            fila.appendChild(col_imagen);
+            fila.appendChild(col_nombre);
+            fila.appendChild(col_precioC);
+            fila.appendChild(col_cant);
+            fila.appendChild(col_apartado);
+
+            tbody.appendChild(fila);
+            
+            document.getElementById("familias").value="Seleccione una familia";
+            document.getElementById("codigoProducto").value="";
+            document.getElementById("nombreProducto").value="";
+            document.getElementById("cantProducto").value="";
+            document.getElementById("precioProducto").value="";
+            document.getElementById("precio_ventaProducto").value="";
+            document.getElementById("imagen").value="";
+            document.getElementById("descripcionProducto").value="";
+
+        }else{
+            alert("No deje elementos vacio");
+        }
 
     }else{
         alert("Seleccione una familia");
